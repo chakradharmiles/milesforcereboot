4d892179d948dc4de8207de9b40ad8e53b6ee224
miles <chakradhar.gollapudi@wikischool.com>
2017-08-09 16:22:36 +0530

    script changes
    changes

--- test/topics.php
+++ test/topics.php
@@ -16,3 +16,20 @@
        window.open('pdftest.php?name='+filename,'mywindow','width=1000,height=1000');
 });
 </script>
+
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script type="text/javascript" language="javascript">
+ $("#clickwin").click(function(){
+      var filename=$(this).attr("filename");
+       window.open('pdftest.php?name='+filename,'mywindow','width=1000,height=1000');
+});
+</script>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script type="text/javascript" language="javascript">
+ $("#clickwin").click(function(){
+      var filename=$(this).attr("filename");
+       window.open('pdftest.php?name='+filename,'mywindow','width=1000,height=1000');
+});
+</script>
